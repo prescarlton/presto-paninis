9039a8992aa5033db112e5d683ba4da7248e54f8
Preston <prescarlton@gmail.com>
2023-08-25 00:07:23 -0400

    removed example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { ordersRouter } from "./routers/orders";
 
 /**
@@ -8,7 +7,6 @@ import { ordersRouter } from "./routers/orders";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   orders: ordersRouter,
 });
 
